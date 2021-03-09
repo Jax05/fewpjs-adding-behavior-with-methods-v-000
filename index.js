7a9d8773a6616d1b12ee94d928b73276232c4cb7
Carley Tripp <carley.tripp@outlook.com>
2021-03-09 22:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Dog {
   }
 
   speak() {
-    console.log(`$(this.name) says woof!`);
+    return `$(this.name) says woof!`;
   }
 }
 
@@ -28,9 +28,9 @@ class Bird {
 
   speak() {
     if(this.sex == male) {
-      console.log(`It's me! $(this.name), the parrot!`);
+      return `It's me! $(this.name), the parrot!`;
     } else {
-      console.log(`$(this.name) says squawk!`);
+      return `$(this.name) says squawk!`;
     };
   }
 }
